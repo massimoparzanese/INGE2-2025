e75bf3f5cac3e870dff3f1fd01b1b040b0fb9008
Santiago Montoya Diodati <entradilla-tarball0i@icloud>
2025-05-25 16:31:07 -0300

    Agregado nuevo endpoint para los autos en todas las sucursales

--- backend/routes/autos/autosInfo.js
+++ backend/routes/autos/autosInfo.js
@@ -2,11 +2,21 @@ import { Router } from "express";
 import { autosRepository } from "./autosRepository.js";
 const autosInfoRouter = Router();
 
-autosInfoRouter.get("/", async (req, res) =>{
+autosInfoRouter.get("/por-sucursal", async (req, res) =>{
     try{
         const {sucursal} = req.body;
         const data = await autosRepository.getSpecifyAutosInfo(sucursal);
         res.send(data)
+    }
+    catch (err){
+        res.send(err);
+    }
+})
+
+autosInfoRouter.get("/todos", async (req, res) => {
+    try{
+        const data = await autosRepository.getAllAutos();
+        res.send(data);
     }
     catch (err){
         res.send(err);
