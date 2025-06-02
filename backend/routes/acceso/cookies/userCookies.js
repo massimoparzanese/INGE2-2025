5f752c1b4a108de9b3440064864303cdf4c9e65c
Franco Turrin <francoturrin1540@mail.com>
2025-06-02 16:52:01 -0300

    Parte del backend

--- backend/routes/acceso/cookies/userCookies.js
+++ backend/routes/acceso/cookies/userCookies.js
@@ -24,4 +24,37 @@ logedUserCookiesRouter.post("/", async (req, res) => {
   }
 });
 
+logedUserCookiesRouter.post("/establecer", async(req, res) =>{
+  try{
+    const{  access_token, refresh_token } = req.body;
+    if (!access_token || !refresh_token) {
+      return res.status(400).json({ message: "Faltan tokens" });
+    }
+
+    const user = await autenticacionRepository.validarToken(access_token);
+    if(!user){
+      return res.status(401).json({message: "Token invalido"});
+    }
+
+    res.cookie("sb-access-token", access_token, {
+      httpOnly: true,
+      secure: false, // true en producción
+      sameSite: "Lax",
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 días
+    });
+
+    res.cookie("sb-refresh-token", refresh_token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "Lax",
+      maxAge: 1000 * 60 * 60 * 24 * 30, // 30 días
+    });
+
+    res.json({message: "Sesion establecida correctamente"})
+  }catch(e){
+    console.error("Error al establecer sesión:", e);
+    res.status(500).json({ message: "Error interno" });
+  }
+});
+
 export default logedUserCookiesRouter;
